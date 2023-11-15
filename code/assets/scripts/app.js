18727045ccb71225bf9c9d4ee32406f5733021ab
kunal kale <kunalkale7080@gmail.com>
2023-11-15 13:48:35 +0530

    modified app.js

--- code/assets/scripts/app.js
+++ code/assets/scripts/app.js
@@ -2,13 +2,46 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
+function getUserNumberInput() {
+  return parseInt(usrInput.value)
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, clacNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${clacNumber}`
+  outputResult(currentResult, calcDescription);
+
+}
+
 function add() {
-  const calcDescription = `${currentResult} + ${userInput.value.toString()}`
-  currentResult = currentResult +  parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
 }
 
 
-
-
 addBtn.addEventListener("click", add);
+subtractbtn.addEventListener("click", subtract);
+multiplybtn.addEventListener("click", multiply);
+dividebtn.addEventListener("click", divide);
--- code/assets/scripts/vendor.js
+++ code/assets/scripts/vendor.js
@@ -1,8 +1,8 @@
-const userInput = document.getElementById('input-number');
+const usrInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
-const subtractBtn = document.getElementById('btn-subtract');
-const multiplyBtn = document.getElementById('btn-multiply');
-const divideBtn = document.getElementById('btn-divide');
+const subtractbtn = document.getElementById('btn-subtract');
+const multiplybtn = document.getElementById('btn-multiply');
+const dividebtn = document.getElementById('btn-divide');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
