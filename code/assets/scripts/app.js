606c22cec451c0514c8ae89dfc7b9c4f33f215b5
kunal kale <kunalkale7080@gmail.com>
2023-11-15 15:17:25 +0530

    modified app.js

--- code/assets/scripts/app.js
+++ code/assets/scripts/app.js
@@ -2,41 +2,43 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
+// Gets input from input field
 function getUserNumberInput() {
   return parseInt(usrInput.value)
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, clacNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${clacNumber}`
-  outputResult(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription); // from vendor file.
 
 }
 
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteOutput("*", initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteOutput("/", initialResult, enteredNumber);
 }
 
